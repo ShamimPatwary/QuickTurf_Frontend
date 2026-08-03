685e640e77776f2f332c19d70f8613719822d74b
Shamim <shamimpatwary002@gmail.com>
2026-08-03 15:36:59 +0600

    booking details

---
+++ src/components/turf-admin/BookingDetailModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Modal from "../common/Modal";
+import Button from "../common/Button";
+import Badge from "../common/Badge";
+import { getInvoiceUrl } from "../../api/publicApi";
